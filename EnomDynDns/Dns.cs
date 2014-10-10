82c0241df9144ce3e277741721ae11b2d70bb99b
brycebpi <bryce.ashey@bpiedu.com>
2014-10-10 10:56:41 -0700

    Initial commit

---
+++ EnomDynDns/Dns.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Http;
+
+namespace EnomDynDns
+{
+    public static class Dns
+    {
+        public static void Update()
+        {
+            var host = GetAppSetting("host");
+            var zone = GetAppSetting("zone");
+            var password = GetAppSetting("domainPassword");
+            var enomServer = GetAppSetting("enomServer");
+
+            var httpClient = new HttpClient();
+            httpClient.GetAsync(new Uri(string.Format(enomServer, host, zone, password))).ContinueWith(
+                (requestTask) =>
+                {
+                    var response = requestTask.Result;
+                    response.EnsureSuccessStatusCode();
+                });
+        }
+
+        private static string GetAppSetting(string key)
+        {
+            var setting = ConfigurationManager.AppSettings[key];
+            if(string.IsNullOrEmpty(setting))
+                throw new ArgumentNullException(string.Format("{0} AppSetting may not be missing, null, nor empty.", key));
+            return setting;
+        }
+    }
+}
---
+++ EnomDynDns/EnomDynDns.cs
@@ -0,0 +1,62 @@
+﻿using System.ServiceProcess;
+
+namespace EnomDynDns
+{
+    public partial class EnomDynDns : ServiceBase
+    {
+        private System.Timers.Timer _timer;
+
+        public EnomDynDns()
+        {
+            InitializeComponent();
+            if (!System.Diagnostics.EventLog.SourceExists("EnomDynDns"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "EnomDynDns", "EnomDynDnsLog");
+            }
+            eventLog1.Source = "EnomDynDns";
+            eventLog1.Log = "EnomDynDnsLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("EnomDynDns Starting...");
+
+            Dns.Update();
+
+            _timer = new System.Timers.Timer(300);
+            _timer.Elapsed += timer_Elapsed;
+            _timer.Enabled = true;
+
+            eventLog1.WriteEntry("EnomDynDns Started...");
+        }
+
+        private void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Dns.Update();
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("EnomDynDns Stopping...");
+            _timer.Enabled = false;
+            eventLog1.WriteEntry("EnomDynDns Stopped...");
+        }
+
+        protected override void OnContinue()
+        {
+            eventLog1.WriteEntry("EnomDynDns Continuing...");
+            base.OnContinue();
+            _timer.Enabled = false;
+            eventLog1.WriteEntry("EnomDynDns Continued...");
+        }
+
+        protected override void OnPause()
+        {
+            eventLog1.WriteEntry("EnomDynDns Pausing...");
+            base.OnContinue();
+            _timer.Enabled = true;
+            eventLog1.WriteEntry("EnomDynDns Paused...");
+        }
+    }
+}
